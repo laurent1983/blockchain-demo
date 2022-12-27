cd917310e4859b4e9f92888005f4f8346d107dc1
laurent1983 <laurent.nicolas7@gmail.com>
2022-12-27 22:04:07 +0300

    donation

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
 import NavbarP from '../components/NavbarP'
+imp
 
 export default function App({ Component, pageProps }) {
   return <>
             <NavbarP />
             <Component {...pageProps} />
+
           </>
 }
